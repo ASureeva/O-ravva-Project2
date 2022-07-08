ccc69ed1af1b3ba254904dd544f89e1d3f24acb3
ASureeva <annasureeva3955@gmail.com>
2022-07-08 18:13:07 +0300

    refactor project

---
+++ Revul/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    private Animator ch_animator;
+
+    private AudioSource stepAudio;
+
+    void Start()
+    {
+        ch_animator = GetComponent<Animator>();
+        stepAudio = GetComponent<AudioSource>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 || vertical != 0)ch_animator.SetBool("state", true);
+        else ch_animator.SetBool("state", false);
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            if (!stepAudio.isPlaying)
+            {
+                stepAudio.Play();
+            }
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else
+        {
+            {
+                stepAudio.Stop();
+            }
+        }
+    }
+}
